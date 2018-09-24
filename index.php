80a34f96871fe3192a92ad5d75fe16bee2467de7
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2018-09-24 18:17:14 +0330

    landing and first look commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php
+echo <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <title>سید محمد هادی طباطبایی - خوش آمدید</title>
+</head>
+<body>
+
+<header>
+    <section class="container">
+        <section class="row mt-15px minHeight no-gutters">
+            <div class="col-lg bgTransition">
+                <a href="#" style="display: inline-block" class="w-100 h-100">
+                    <img src="assets/img/tree.png" alt=""
+                         class="img-fluid h-100 w-100 imgHover">
+                </a>
+            </div>
+            <div class="col-lg bgOrange text-center bgTransition">
+                <h4 class="yekanFont d-block fontSize-15px mt-30px">
+                    <a href="#" class="colorLightOrange linkDefault">رزومه من</a>
+                </h4>
+                <h4 class="corbelFont d-block fontSize-15px">
+                    <a href="#" class="colorLightOrange linkDefault">My CV</a>
+                </h4>
+            </div>
+
+            <div class="col-lg bgBlue bgTransition specialCol">
+                <a href="#" class="d-block text-center mt-30px iconLink">
+                    <img src="assets/img/avatarIcon.png" alt="avatar icon" draggable="false">
+                </a>
+                <h4 class="yekanFont d-block fontSize-15px text-center">
+                    <a href="#" class="colorLightBlue linkDefault">درباره من</a>
+                </h4>
+            </div>
+
+            <div class="col-lg bgGreen text-center bgTransition">
+                <h4 class="yekanFont d-block fontSize-15px mt-30px">
+                    <a href="#" class="colorLightGreen linkDefault">سید محمد هادی طباطبایی</a>
+                </h4>
+                <h4 class="corbelFont d-block fontSize-15px">
+                    <a href="#" class="colorLightGreen linkDefault">Me</a>
+                </h4>
+            </div>
+
+        </section>
+
+        <section class="row mt-15px minHeight no-gutters">
+            <div class="col-lg bgRed text-center bgTransition">
+                <h4 class="yekanFont d-block fontSize-15px mt-30px">
+                    <a href="contact.html" class="colorLightRed linkDefault">ارتباط با من</a>
+                </h4>
+                <h4 class="corbelFont d-block fontSize-15px">
+                    <a href="contact.html" class="colorLightRed linkDefault">Contact Me</a>
+                </h4>
+            </div>
+
+            <div class="col-lg bgOrange bgTransition specialCol">
+                <a href="#" class="d-block text-center mt-30px iconLink">
+                    <img src="assets/img/calender.png" alt="calender icon" draggable="false">
+                </a>
+                <h4 class="yekanFont d-block fontSize-15px text-center">
+                    <a href="#" class="colorLightOrange linkDefault">تقویم</a>
+                </h4>
+            </div>
+
+            <div class="col-lg bgBlue text-center bgTransition">
+                <h4 class="yekanFont d-block fontSize-15px mt-30px">
+                    <a href="#" class="colorLightBlue linkDefault">تحصیلات</a>
+                </h4>
+                <h4 class="corbelFont d-block fontSize-15px">
+                    <a href="#" class="colorLightBlue linkDefault">Education</a>
+                </h4>
+            </div>
+
+
+            <div class="col-lg text-center bgTransition">
+                <a href="#" style="display: inline-block" class="w-100 h-100 ">
+                    <img src="assets/img/avatar.jpg" alt="hadi image" class="img-fluid h-100 w-100 imgHover">
+                </a>
+            </div>
+
+        </section>
+    </section>
+</header>
+<footer>
+    <section class="container">
+        <section class="row no-gutters minSmallHeight bgLightGreen mt-15px text-center">
+            <a href="" class="d-block mx-auto my-auto yekanFont" style="font-size:20px;">طراحی و کدنویسی : سید محمد هادی
+                طباطبایی</a>
+        </section>
+    </section>
+</footer>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"-->
+<!--integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"-->
+<!--crossorigin="anonymous"></script>-->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"-->
+<!--integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"-->
+<!--crossorigin="anonymous"></script>-->
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"-->
+<!--integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"-->
+<!--crossorigin="anonymous"></script>-->
+</body>
+</html>
+
+HTML;
+?>
